7508d76f909643d18e13aabd57c7d52fbd074574
Davi <davicamiloribeiro@gmail.com>
2022-01-26 22:01:45 -0300

    Classe Calculadora para exemplo

---
+++ Models/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploDelegates.Models
+{
+    internal class Calculadora
+    {
+        public static void Somar(int x, int y) {
+            Console.WriteLine($"Adição: {x+y}");
+        }
+
+        public static void Subtrair(int x, int y) {
+            Console.WriteLine($"Subtração: {x-y}");
+        }
+    }
+}
